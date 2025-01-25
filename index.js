36b12108ed5235050a7dceeb0bec93374cf2011a
Md Rokonuzzaman <rokonjust@gmail.com>
2025-01-25 16:59:01 +0600

    Get User Role By Email Added

--- index.js
+++ index.js
@@ -255,6 +255,13 @@ async function run() {
       res.send(result);
     });
 
+    // get user role
+    app.get("/users/role/:email", async (req, res) => {
+      const email = req.params.email;
+      const result = await usersCollection.findOne({ email });
+      res.send({ role: result?.role });
+    });
+
     await client.db("admin").command({ ping: 1 });
     console.log(
       "Pinged your deployment. You successfully connected to MongoDB!"
